ad822e8577c6140f5611e98186ae9977c3b022d8
Ajeet Gupta <ajeetkg@bu.edu>
2017-11-19 20:14:53 -0800

    MAINT: Added the  basic springboot aplication file

---
+++ README.md
@@ -0,0 +1,2 @@
+# kerence
+Aims to build kerence management application in the crypto-currency space by Mar 31, 2018
---
+++ kerence-lookup/src/main/java/com/qlective/kerence/KerenceApplication.java
@@ -0,0 +1,10 @@
+package com.qlective.kerence;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KerenceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
