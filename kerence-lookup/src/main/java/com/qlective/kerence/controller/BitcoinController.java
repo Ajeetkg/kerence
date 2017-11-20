d38c4e70ee92e69e0d1dce3ecfac487b8a66653b
Ajeet Gupta <ajeetkg@bu.edu>
2017-11-19 21:01:51 -0800

    MAINT: Added hte basic swaggerconfig file

---
+++ kerence-lookup/src/main/java/com/qlective/kerence/controller/BitcoinController.java
@@ -0,0 +1,13 @@
+package com.qlective.kerence.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by testmac on 11/19/17.
+ */
+
+@RestController
+public class BitcoinController {
+
+
+}
