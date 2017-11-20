887734f99c203eeca495c8d30c00f117ed8a61f6
Ajeet Gupta <ajeetkg@bu.edu>
2017-11-19 20:54:15 -0800

    MAINT: Added the basic springbootapplication

--- kerence-lookup/src/main/java/com/qlective/kerence/KerenceApplication.java
+++ kerence-lookup/src/main/java/com/qlective/kerence/KerenceApplication.java
@@ -1,10 +1,12 @@
 package com.qlective.kerence;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KerenceApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(KerenceApplication.class, args);
     }
 }
